af4c80da98535d506d435e9b52467a36f0148ab3
kms435 <kms435@cornell.edu>
2017-11-28 20:59:59 -0500

    studd

--- server.js
+++ server.js
@@ -1,56 +1,46 @@
 var express = require('express');
-var app = express.Router();
+var app = express();
+
+var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// index page 
 app.get('/', function(req, res) {
-	res.status(200).send('nothing to see here!');
+	res.render('./result', data);
+	// res.status(200).send('nothing to see here!');
 });
 
-app.get('/result', function(req, res) {
-	
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', '*');
+// app.get('/result', function(req, res) {
 
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// 	// var java = spawn('java', ['java', '-cp', '"*"', '-Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP',
+// 	// 	'-annotators', 'tokenize,ssplit,pos,lemma,ner,parse,dcoref', req.query.text]);
+// 	// var spawn = require('child_process').spawn;
+// 	// var java = spawn('java', ['-mx4g', '-cp', '"*"', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer']);
 
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+// 	// var spawn = require('child_process').spawn;
+// 	// var java = spawn('java', ['edu.stanford.nlp.parser.nndep.DependencyParser',
+// 	// 	'-model', 'modelOutputFile.txt.gz', '-textFile', '-', '-outFile', '-']);
 
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
+// 	// java.stdin.write('Hi, my name is Karrie'); 
 
-	// var java = spawn('java', ['java', '-cp', '"*"', '-Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP',
-	// 	'-annotators', 'tokenize,ssplit,pos,lemma,ner,parse,dcoref', req.query.text]);
-	// var spawn = require('child_process').spawn;
-	// var java = spawn('java', ['-mx4g', '-cp', '"*"', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer']);
+// 	// java.stdout.on('data', function(data) {
+// 	// 	console.log(data.toString());
+// 	// });
 
-	// var spawn = require('child_process').spawn;
-	// var java = spawn('java', ['edu.stanford.nlp.parser.nndep.DependencyParser',
-	// 	'-model', 'modelOutputFile.txt.gz', '-textFile', '-', '-outFile', '-']);
+// 	var spawn = require('child_process').spawn;
+// 	var python = spawn('python', ['./analyze.py', req.query.text]);
 
-	// java.stdin.write('Hi, my name is Karrie'); 
+// 	python.stdout.on('data', function(data) {
+// 		var data = {
+// 			locals: {
+// 				text: data.toString()
+// 			}
+// 		}
+// 		res.render('./result', data);
+// 	});
+// });
 
-	// java.stdout.on('data', function(data) {
-	// 	console.log(data.toString());
-	// });
-
-	var spawn = require('child_process').spawn;
-	var python = spawn('python', ['./analyze.py', req.query.text]);
-
-	python.stdout.on('data', function(data) {
-		var data = {
-			locals: {
-				text: data.toString()
-			}
-		}
-		res.render('./result', data);
-	});
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
-
-app.listen(8080);
-console.log('listening on port 8080');

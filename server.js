12c3ff0f18bd8260fc9c3db14ff58acceb1426d0
kms435 <kms435@cornell.edu>
2017-11-28 21:46:13 -0500

    vader

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/result', function(req, res) {
 	var spawn = require('child_process').spawn;
 	var python = spawn('python', ['./analyze.py', req.query.text]);
 	python.stdout.on('data', function(data) {
@@ -19,22 +19,22 @@ app.get('/', function(req, res) {
 	});
 });
 
-app.get('/result', function(req, res) {
-	// var java = spawn('java', ['java', '-cp', '"*"', '-Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP',
-	// 	'-annotators', 'tokenize,ssplit,pos,lemma,ner,parse,dcoref', req.query.text]);
-	// var spawn = require('child_process').spawn;
-	// var java = spawn('java', ['-mx4g', '-cp', '"*"', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer']);
+// app.get('/result', function(req, res) {
+// 	// var java = spawn('java', ['java', '-cp', '"*"', '-Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP',
+// 	// 	'-annotators', 'tokenize,ssplit,pos,lemma,ner,parse,dcoref', req.query.text]);
+// 	// var spawn = require('child_process').spawn;
+// 	// var java = spawn('java', ['-mx4g', '-cp', '"*"', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer']);
 
-	// var spawn = require('child_process').spawn;
-	// var java = spawn('java', ['edu.stanford.nlp.parser.nndep.DependencyParser',
-	// 	'-model', 'modelOutputFile.txt.gz', '-textFile', '-', '-outFile', '-']);
+// 	// var spawn = require('child_process').spawn;
+// 	// var java = spawn('java', ['edu.stanford.nlp.parser.nndep.DependencyParser',
+// 	// 	'-model', 'modelOutputFile.txt.gz', '-textFile', '-', '-outFile', '-']);
 
-	// java.stdin.write('Hi, my name is Karrie'); 
+// 	// java.stdin.write('Hi, my name is Karrie'); 
 
-	// java.stdout.on('data', function(data) {
-	// 	console.log(data.toString());
-	// });
-});
+// 	// java.stdout.on('data', function(data) {
+// 	// 	console.log(data.toString());
+// 	// });
+// });
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);

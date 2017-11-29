fa9dcf65615afc04831f1c504af79fe507af23f0
kms435 <kms435@cornell.edu>
2017-11-28 21:16:56 -0500

    stuff

--- server.js
+++ server.js
@@ -12,35 +12,36 @@ app.get('/', function(req, res) {
 	// console.log("hellooooo");
 });
 
-// app.get('/result', function(req, res) {
+app.get('/result', function(req, res) {
+	res.render('./temp');
 
-// 	// var java = spawn('java', ['java', '-cp', '"*"', '-Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP',
-// 	// 	'-annotators', 'tokenize,ssplit,pos,lemma,ner,parse,dcoref', req.query.text]);
-// 	// var spawn = require('child_process').spawn;
-// 	// var java = spawn('java', ['-mx4g', '-cp', '"*"', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer']);
+	// var java = spawn('java', ['java', '-cp', '"*"', '-Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP',
+	// 	'-annotators', 'tokenize,ssplit,pos,lemma,ner,parse,dcoref', req.query.text]);
+	// var spawn = require('child_process').spawn;
+	// var java = spawn('java', ['-mx4g', '-cp', '"*"', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer']);
 
-// 	// var spawn = require('child_process').spawn;
-// 	// var java = spawn('java', ['edu.stanford.nlp.parser.nndep.DependencyParser',
-// 	// 	'-model', 'modelOutputFile.txt.gz', '-textFile', '-', '-outFile', '-']);
+	// var spawn = require('child_process').spawn;
+	// var java = spawn('java', ['edu.stanford.nlp.parser.nndep.DependencyParser',
+	// 	'-model', 'modelOutputFile.txt.gz', '-textFile', '-', '-outFile', '-']);
 
-// 	// java.stdin.write('Hi, my name is Karrie'); 
+	// java.stdin.write('Hi, my name is Karrie'); 
 
-// 	// java.stdout.on('data', function(data) {
-// 	// 	console.log(data.toString());
-// 	// });
+	// java.stdout.on('data', function(data) {
+	// 	console.log(data.toString());
+	// });
 
-// 	var spawn = require('child_process').spawn;
-// 	var python = spawn('python', ['./analyze.py', req.query.text]);
+	// var spawn = require('child_process').spawn;
+	// var python = spawn('python', ['./analyze.py', req.query.text]);
 
-// 	python.stdout.on('data', function(data) {
-// 		var data = {
-// 			locals: {
-// 				text: data.toString()
-// 			}
-// 		}
-// 		res.render('./result', data);
-// 	});
-// });
+	// python.stdout.on('data', function(data) {
+	// 	var data = {
+	// 		locals: {
+	// 			text: data.toString()
+	// 		}
+	// 	}
+	// 	res.render('./result', data);
+	// });
+});
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);

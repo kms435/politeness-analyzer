a4f3b318578a325e13948b4efda8227316194ff6
kms435 <kms435@cornell.edu>
2017-12-03 18:06:47 -0500

    itinal

--- server.js
+++ server.js
@@ -1,46 +1,41 @@
 var express = require('express');
 var app = express();
 
+var WatsonNLU = require('watson-developer-cloud/natural-language-understanding/v1.js');
+var natural_language_understanding = new WatsonNLU({
+	'username': 'a314bf94-dd1d-461b-a851-c76eeea32057',
+	'password': 'gZFirr37Ln4j',
+	'version_date': '2017-02-27'
+});
+
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.render('./index2');
+	res.render('./index');
 });
 
-app.get('/result', function(req, res) {
-	var spawn = require('child_process').spawn;
-	var python = spawn('python', ['./analyze.py', req.query.text]);
-	console.log("python spawned");
-	python.stdout.on('data', function(data) {
-		var data = {
-			locals: {
-				text: data.toString()
-			}
+app.get('/analyze', function(req, res) {
+	var query = {
+		'text': req.query.text,
+		'features': {
+			'sentiment': {},
+			'emotion': {}
 		}
-		res.render('./index', data);
+	}
+	natural_language_understanding.analyze(query, function(err, data) {
+		if (err) {
+			console.log('error:', err);
+		} else {
+			res.render('./analyze', {
+				data: JSON.stringify(data)
+			});
+		}
 	});
 });
 
-// app.get('/result', function(req, res) {
-// 	// var java = spawn('java', ['java', '-cp', '"*"', '-Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP',
-// 	// 	'-annotators', 'tokenize,ssplit,pos,lemma,ner,parse,dcoref', req.query.text]);
-// 	// var spawn = require('child_process').spawn;
-// 	// var java = spawn('java', ['-mx4g', '-cp', '"*"', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer']);
-
-// 	// var spawn = require('child_process').spawn;
-// 	// var java = spawn('java', ['edu.stanford.nlp.parser.nndep.DependencyParser',
-// 	// 	'-model', 'modelOutputFile.txt.gz', '-textFile', '-', '-outFile', '-']);
-
-// 	// java.stdin.write('Hi, my name is Karrie'); 
-
-// 	// java.stdout.on('data', function(data) {
-// 	// 	console.log(data.toString());
-// 	// });
-// });
-
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+	console.log('Our app is running on http://localhost:' + port);
 });
